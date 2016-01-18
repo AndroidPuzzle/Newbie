949b17c03b97949810382f23cf17d69b24b544f0
mingfei <mingfei.net@gmail.com>
2016-01-18 18:57:03 +0800

    Update

--- activity_lifecycle/src/main/java/net/mingfei/android/puzzle/easy/activity_lifecycle/MainActivity.java
+++ activity_lifecycle/src/main/java/net/mingfei/android/puzzle/easy/activity_lifecycle/MainActivity.java
@@ -1,16 +1,77 @@
 package net.mingfei.android.puzzle.easy.activity_lifecycle;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import net.mingfei.android.puzzle.activity_lifecycle.app.R;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    private Button second;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+        Log.e("demo", "on create...");
+    }
+
+    private void initView() {
+        second = (Button) findViewById(R.id.second_activity);
+
+        second.setOnClickListener(this);
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.e("demo", "on restart...");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.e("demo", "on start...");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.e("demo", "on resume...");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.e("demo", "on pause...");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.e("demo", "on stop...");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.e("demo", "on destroy...");
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent;
+        switch (v.getId()) {
+            case R.id.second_activity:
+                intent = new Intent(this, SecondActivity.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
     }
 }
---
+++ activity_lifecycle/src/main/java/net/mingfei/android/puzzle/easy/activity_lifecycle/SecondActivity.java
@@ -0,0 +1,17 @@
+package net.mingfei.android.puzzle.easy.activity_lifecycle;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * @author mingfei.net@Gmail.com
+ *         18:45, 1/18/16.
+ */
+public class SecondActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
