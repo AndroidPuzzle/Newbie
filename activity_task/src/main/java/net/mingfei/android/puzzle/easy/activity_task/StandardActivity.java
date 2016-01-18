15127d8e5b27b6a686931555b597d26556fe1aa3
mingfei <mingfei.net@gmail.com>
2016-01-18 17:45:39 +0800

    Initial commit

---
+++ activity_task/src/main/java/net/mingfei/android/puzzle/easy/activity_task/StandardActivity.java
@@ -0,0 +1,15 @@
+package net.mingfei.android.puzzle.easy.activity_task;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class StandardActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_standard);
+    }
+}
